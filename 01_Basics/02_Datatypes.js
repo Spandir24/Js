d261e7ceb5b7f0314c46a43eac51974341629890
Spandir24 <pandirsana@gmail.com>
2026-02-25 01:54:20 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,19 @@
+let name= " Sana"
+let age= 18
+let isloggedIn= false
+let state;   //undefined
+
+//DATATYPES:
+// -> Primitive datatypes:-
+    //number= 2^53
+    //bigint=
+    //string= "" OR ''(any)
+    //boolean= true/false
+    //null [Intentionally empty] (also a standalone value in JS)
+    //undefined [Unintentionally empty]- when a value is not assigned
+    //symbols => when we want to talk about somethng unique (most used in react)
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
