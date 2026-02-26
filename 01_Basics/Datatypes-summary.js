eed65864657c707f318286b9048beaf954b5d205
Spandir24 <pandirsana@gmail.com>
2026-02-26 01:51:29 +0000

    datatypes summary

---
+++ 01_Basics/Datatypes-summary.js
@@ -0,0 +1,33 @@
+//PRIMITIVE DATATYPES:
+/*const score= 100
+const scoreValue= 100.3
+const isLoggedIn= false
+const outsideTemp= null
+let userEmail= undefined     // OR can write it as- (both same):  let userEmail
+*/
+
+//usage and example of symbol:
+/*onst id= Symbol('123')
+const anotherId= Symbol('123')*/
+// console.log(id==anotherId)
+
+// const bigNumber = 42542362321334n
+
+
+//NON-PRIMITIVE DATATYPES:
+/*const heros= ["Shaktiman", "Naagraj", "Doga"]     //ARRAYS*/
+
+//objects
+/*let myObj= {
+    name: "Sana",     //any datatype can be used
+    age: 22
+}*/
+
+// FUNC- can be treated as varaiables here
+/*const myFunction= function(){
+    console.log("Hello World!")
+}
+*/
+
+//HOW TO IDENTIFY DATATYPES: typeof
+// console.log(typeof bigNumber)
