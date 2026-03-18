70b39dccaad17ca7ec8237303bb3e8aec206ed80
Spandir24 <pandirsana@gmail.com>
2026-03-18 12:07:09 +0000

    maths

--- 01_Basics/06_nums&maths.js
+++ 01_Basics/06_nums&maths.js
@@ -14,5 +14,19 @@ console.log(balance.toFixed(2))*/             // fixed to 2 decimal places
 /*const otherNumber= 23.8966
 console.log(otherNumber.toPrecision(4))*/
 
-const hundreds= 1000000;
-console.log(hundreds.toLocaleString('en-IN'));
+/*const hundreds= 1000000;
+console.log(hundreds.toLocaleString('en-IN'));*/
+
+
+//Maths
+/*console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.9));
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+*/
+
+/*const min= 10;
+const max=20;
+console.log(Mathfloor(Math.random() * (max-min+1)) + min);*/
