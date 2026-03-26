34c65896d17d2b36cfd71b5cc5f14badf70bf0cd
Spandir24 <pandirsana@gmail.com>
2026-03-26 17:20:00 +0000

    objects

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,5 +1,6 @@
 //2 ways of creating an empty object-
-//const tinderUser= new Object()      //singleton object
+
+//const tinderUser= new Object()      //singleton object    //Object constructor
 const tinderUser= {}                  //Non- singleton object
 
 tinderUser.id= "123abc"
@@ -23,9 +24,42 @@ const regularUser= {
 //to combine objects- (just like arrays)
 const obj1= {1: "a", 2: "b"}
 const obj2= {3: "c", 4: "d"}
-// similar problem as array arises here: "obj inside object"-
-//const obj3= {obj1, obj2}
-//to avoid this problem:
-console.log(obj3)
+const obj4= {5: "e", 6: "f"}
+//way-1) similar problem as array arises here: "obj inside object"-
+/*const obj3= {obj1, obj2}
+console.log(obj3) */
 
-//Object constructor-
+//way-2) to avoid this problem-                       //less preferred
+/*const obj3= Object.assign({}, obj1, obj2, obj4)
+console.log(obj3) */     
+
+//way-3) the 'SPREAD' method-                       //90% usage
+/*const obj3= {...obj1, ...obj2}
+console.log(obj3); */
+
+//multiple 'objects' as elements inside an array
+/*const users= [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+]
+console.log(users[1].email)  */      //STEP: 1) an array => thus indexed,
+                                    // then, 2) object => accesed via (.) dot notation
+
+//***********
+console.log(tinderUser)
+//imp methods-
+//console.log(Object.keys(tinderUser))       //returns an array*** (V.IMP)
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))   //each 'key:value' pair is made an array.
+//to ask whether a particular value(ie., key) exists in an object-
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));            
