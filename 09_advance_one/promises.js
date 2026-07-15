ba0796d99401596438c81800a80cdb166230cf0d
Spandir24 <pandirsana@gmail.com>
2026-07-15 15:46:29 +0000

    Pending changes exported from your codespace

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,77 @@
+// majority times we are going to consume promises here, but promises are created also.
+//Firstly, we'll learn to make diff types of promises:
+/*const promiseOne= new Promise(function(resolve, reject) {
+    // Do an async task: DB calls, cryptography related, network related calls
+    // here we consider:
+    setTimeout(function() {
+        console.log("ASYNC task is complete");
+        resolve()
+    }, 1000)
+})
+
+//now we need to use the above created promise
+promiseOne.then(function(){
+    console.log("Promise consumed")
+})
+// after doing console.log at this stage only setTimeout() executes. But 'Promise consumed' does not display. 
+// WHY?- becz we did not connect resolve an .then  via => resolve()
+
+// NOTE: that 'Promise consumed' will never come first becz 1st the async task was completed then it was resolved.
+*/
+
+// now the same (above) task can be done w/o storing the promise in a variable: in a single part
+/*new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        console.log("ASYNC task 2");
+        resolve();     // when this task will be completed then, resolve() will automatically  inform .then()
+    }, 1000)           // this can be any async task
+
+}).then(function(){
+    console.log("ASYNC 2 resolved!");
+})
+*/
+
+//Suppose, promise ka callback func goes & gets some data from network/ file system- we need to pass that data to .then()  [which we said, recieves the return value of the task executed by promise]
+/*const promiseThree= new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve( {username: "Chai", email: "chai@example.com"} )   // thus, that data is passed thru resolve() as a parameter whic can be array, obj, func(). Majority times it will be an object
+    }, 1000)
+})
+promiseThree.then(function(user) {
+    console.log(user); 
+})
+*/
+
+// we are performing error based checking
+/*const promiseFour= new Promise(function(resolve, reject){
+    setTimeout(function() {
+        let error= true
+        // let error= false
+        if(!error){
+            resolve( {username: "Sana", password: "123"})
+        } else{
+            reject('ERROR: Something went wrong')  // 'reject' is used to give error
+        }
+    }, 1000)
+})
+
+//how callback hell is avoided/ reduced: [we can use any no of .then() & .catch()]- // .then for taking values returned by the promise() & '.catch()' to catch errors
+promiseFour
+.then( (user) => {
+    console.log(user);
+    return user.username;    // but where is this data being returned to?- variable mn, lets chk! => we chkd: aise toh kaam nhi hota
+    // we do chaining of .then() and also need to write .catch() now
+    
+}).then( (myUsername) => {
+    console.log(myUsername);
+    
+}).catch(function(error){
+    console.log(error);
+    
+// now, to get a confirmation(ie it is executed alwys) of a promise ie-  resolve/ reject case hai ye finally?
+}).finally( () => console.log("The promise is either resolved or rejected."));
+*/
+
+const promiseFive= new Promise(function(resolve, reject) {
+    
+})
